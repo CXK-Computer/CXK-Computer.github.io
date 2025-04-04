d305b0728873cb199131d282e8315c097fff5bc2
Amtei <tangtianyu114514@outlook.com>
2025-04-04 20:58:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,39 +2,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-window.onload = function() {
-
-const hourHand = document.querySelector('.hourHand');
-    const minuteHand = document.querySelector('.minuteHand');
-    const secondHand = document.querySelector('.secondHand');
-    const time = document.querySelector('.time');
-    const clock = document.querySelector('.clock');
-    const audio = document.querySelector('.audio');
-
-    function setDate(){
-        const today = new Date();
-        
-        const second = today.getSeconds();
-        const secondDeg = ((second / 60) * 360) + 360; 
-        secondHand.style.transform = `rotate(${secondDeg}deg)`;
-      
-        audio.play();
-        
-        const minute = today.getMinutes();
-        const minuteDeg = ((minute / 60) * 360); 
-        minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
-
-        const hour = today.getHours();
-        const hourDeg = ((hour / 12 ) * 360 ); 
-        hourHand.style.transform = `rotate(${hourDeg}deg)`;
-        
-        time.innerHTML = '<span>' + '<strong>' + hour + '</strong>' + ' : ' + minute + ' : ' + '<small>' + second +'</small>'+ '</span>';
-
-        }
-  
-    setInterval(setDate, 1000);
- 
-}
 
     const particlesContainer = document.getElementById('particles-js');
     if (typeof particlesJS !== 'undefined' && particlesContainer) {
@@ -77,6 +44,40 @@ const hourHand = document.querySelector('.hourHand');
             });
         }
     }
+    window.onload = function() {
+
+const hourHand = document.querySelector('.hourHand');
+    const minuteHand = document.querySelector('.minuteHand');
+    const secondHand = document.querySelector('.secondHand');
+    const time = document.querySelector('.time');
+    const clock = document.querySelector('.clock');
+    const audio = document.querySelector('.audio');
+
+    function setDate(){
+        const today = new Date();
+        
+        const second = today.getSeconds();
+        const secondDeg = ((second / 60) * 360) + 360; 
+        secondHand.style.transform = `rotate(${secondDeg}deg)`;
+      
+        audio.play();
+        
+        const minute = today.getMinutes();
+        const minuteDeg = ((minute / 60) * 360); 
+        minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+
+        const hour = today.getHours();
+        const hourDeg = ((hour / 12 ) * 360 ); 
+        hourHand.style.transform = `rotate(${hourDeg}deg)`;
+        
+        time.innerHTML = '<span>' + '<strong>' + hour + '</strong>' + ' : ' + minute + ' : ' + '<small>' + second +'</small>'+ '</span>';
+
+        }
+  
+    setInterval(setDate, 1000);
+ 
+}
+
 
     const timeEl = document.getElementById('current-time');
     const noticeEl = document.getElementById('time-notice');
